b1ec9d5b00b7b93447e236cf4ec32894e4858fc1
Amonkey00 <892542697@qq.com>
2022-05-06 00:47:30 +0800

    group create逻辑fix

--- src/main/java/com/example/accountbook/controller/GroupController.java
+++ src/main/java/com/example/accountbook/controller/GroupController.java
@@ -1,19 +1,34 @@
 package com.example.accountbook.controller;
 
 import com.example.accountbook.entity.Group;
+import com.example.accountbook.entity.Role;
+import com.example.accountbook.enums.RoleStatusEnum;
+import com.example.accountbook.model.PageResult;
+import com.example.accountbook.service.GroupService;
+import com.example.accountbook.service.RoleService;
 import com.example.accountbook.utils.JsonResult;
+import com.example.accountbook.utils.JwtUtil;
 import com.example.accountbook.vo.group.GroupCreateReqVo;
+import io.jsonwebtoken.Claims;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/group")
 public class GroupController {
 
+    @Autowired
+    GroupService groupService;
+    @Autowired
+    RoleService roleService;
+
+    /**
+     * 创建group
+     */
     @PostMapping("/create")
     public JsonResult create(
             @RequestBody GroupCreateReqVo reqVo
@@ -22,11 +37,70 @@ public class GroupController {
         if (group == null) {
             return new JsonResult(-1,"Group信息有误");
         }
-        return new JsonResult(-1,"Error");
+
+        // 1. check exists
+        Group existsGroup = groupService.getGroupByName(reqVo.getGroupName());
+        if (existsGroup != null) {
+            return new JsonResult(-1,"Group name已经存在");
+        }
+        // 2. insert
+        int flag = groupService.addGroup(group);
+        if (flag <= 0) {
+            return new JsonResult(-1, "group Insert失败");
+        }
+        // 3. insert role
+        try {
+            group = groupService.getGroupByName(reqVo.getGroupName());
+            Role role = new Role(reqVo.getCreatorId(), group.getGId(), RoleStatusEnum.CREATOR.getCode());
+            roleService.addRole(role);
+            return new JsonResult();
+        }catch (Exception e) {
+            e.printStackTrace();
+            return new JsonResult(-1,"组权限设置失败");
+        }
     }
 
+    /**
+     * 查询group
+     */
+    @PostMapping("/update")
+    public JsonResult update(
+            @RequestBody Group group,
+            HttpServletRequest request
+    ) {
+        String token = request.getHeader("token");
+        Integer userId = parseToken2Id(token);
+        if (!checkGroupRole(userId,group.getGId(),RoleStatusEnum.ADMIN.getCode())) {
+            return new JsonResult(-1,"update失败。没有该权限");
+        }
+        int flag = groupService.updateGroup(group);
+        if (flag > 0) {
+            return new JsonResult();
+        }
+        return new JsonResult(-1,"Group 更新失败");
+    }
 
+    @GetMapping("/get")
+    public JsonResult get(
 
+            @RequestParam Integer groupId
+    ) {
+        Group group = groupService.getGroupById(groupId);
+        if (group == null) {
+            return new JsonResult(-1, "不存在该group");
+        }
+        return new JsonResult(group);
+    }
+
+    @GetMapping("/getList")
+    public JsonResult getList(
+            @RequestParam(value = "userId", required = false) Integer userId,
+            @RequestParam("start") Integer start,
+            @RequestParam("size") Integer size
+    ) {
+        PageResult<Group> groupList = groupService.getGroupList(userId, start, size);
+        return new JsonResult(groupList);
+    }
 
 
     // ================ Utils =================
@@ -40,5 +114,23 @@ public class GroupController {
         Group group = new Group();
         BeanUtils.copyProperties(reqVo,group);
         return group;
+    }
+
+
+    private Integer parseToken2Id(String token) {
+        Integer userId = -1;
+        try {
+            Claims claims = JwtUtil.parseJwt(token);
+            userId = (Integer) claims.get("userId");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userId;
+    }
+    private boolean checkGroupRole(Integer userId, Integer groupId, Integer roleCode) {
+        Role role = roleService.getRole(groupId, userId);
+        // Query role and check code
+        if (role == null || role.getStatus() < roleCode) return false;
+        return true;
     }
 }
--- src/main/java/com/example/accountbook/entity/Role.java
+++ src/main/java/com/example/accountbook/entity/Role.java
@@ -4,13 +4,22 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @TableName("group_role")
+@NoArgsConstructor
 public class Role {
     @TableId(value="id", type= IdType.AUTO)
     private Integer id;
+
     private Integer userId;
     private Integer groupId;
     private Integer status;
+
+    public Role(Integer userId, Integer groupId, Integer status) {
+        this.userId = userId;
+        this.groupId = groupId;
+        this.status = status;
+    }
 }
